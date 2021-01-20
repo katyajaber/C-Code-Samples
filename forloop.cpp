ecbbdaf34f019fb16ec9c0d056b4c49485241211
katyajaber <katyajaber@hotmail.de>
2021-01-20 11:59:20 +0100

    Update forloop.cpp

--- forloop.cpp
+++ forloop.cpp
@@ -10,9 +10,8 @@ int main()
    {
       cout << "2^" << i << "=" << ( 1 << i ) << endl;
 
-      // 1 << i bedeutet, dass die Zahl 1 i-mal binaer
-      // geshiftet wird; dies bewirkt eine i-malige
-      // Multiplikation mit 2
+      // 1 << i : die Zahl 1 wirdi-mal binaer geshiftet
+      // bewirkt eine i-malige Multiplikation mit 2
    }
 
    return 0;
